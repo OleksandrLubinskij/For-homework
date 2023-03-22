29998e732b755d4129abfeedf5c8c930dd937447
Sasha <sasalubinskij47@gmail.com>
2023-03-22 17:22:10 +0200

    Read file

--- Programe.py
+++ Programe.py
@@ -1,5 +1,11 @@
 print('Hello User!')
 
+try:
+    with open('save-to-file.txt', 'r', encoding='utf-8') as file:
+        print(file.read())
+except FileNotFoundError:
+    print('File not found')
+
 number = None
 while True:
     a = int(input('\n1 - Set\n2 - Null\n3 - Exit\n\nChoose action: '))
