2f30a167e8d37e310d0d3af3f8068d61c09e1993
Sasha <sasalubinskij47@gmail.com>
2023-03-22 16:52:33 +0200

    Program functionality

--- Programe.py
+++ Programe.py
@@ -3,3 +3,13 @@ print('Hello User!')
 number = None
 while True:
     a = int(input('\n1 - Set\n2 - Null\n3 - Exit\n\nChoose action: '))
+    if a == 1:
+        num = int(input('Enter number: '))
+        number = num
+        print(number)
+    elif a == 2:
+        number = 0
+        print(number)
+    elif a == 3:
+        print('Bye!')
+        break
